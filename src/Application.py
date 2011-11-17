16c6439ba4158d9efff5f332a8eca5d55463cfee
AJ Alves <wga_aj@hotmail.com>
2011-11-17 11:42:31 -0300

    first commit

---
+++ src/Application.py
@@ -0,0 +1,10 @@
+'''
+Created on Nov 17, 2011
+
+@author: zerokol
+'''
+from direct.showbase.ShowBase import ShowBase
+
+class Application(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
