82328c00a309b4b3ed641d1fa6b6b1fe0de3d4fd
AJ Alves <zerokol@ymail.com>
2011-12-20 12:04:35 -0300

    ok

--- src/Application.py
+++ src/Application.py
@@ -4,38 +4,36 @@ Created on Nov 17, 2011
 @author: zerokol
 '''
 from direct.showbase.ShowBase import ShowBase
-from direct.actor.Actor import Actor
-from panda3d.core import Vec3
-from direct.interval.IntervalGlobal import *
-from FollowCam import FollowCam
+from panda3d.core import *
+
+vertices = [Vec3(1, 0, 1), Vec3(-1, 0, 1), Vec3(-1, 0, -1), Vec3(1, 0, -1)]
+texcoords = [Vec2(1, 1), Vec2(0, 1), Vec2(0, 0), Vec2(1, 0)]
 
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         
-        self.world = loader.loadModel("environment")
-        self.world.reparentTo(render)
-        self.world.setScale(0.5)
-        self.world.setPos(-8, 80, 0)
+        format = GeomVertexFormat.getV3t2()
+        geomData = GeomVertexData("box", format, Geom.UHStatic)
+        vertexWriter = GeomVertexWriter(geomData, "vertex")
+        uvWriter = GeomVertexWriter(geomData, "texcoord")
         
-        self.panda = Actor("panda", {"walk": "panda-walk"})
-        self.panda.reparentTo(render)
-        self.panda.setHpr(270, 0, 0)
-        self.panda.loop("walk")
+        for pos, tex in zip(vertices, texcoords):
+            vertexWriter.addData3f(pos)
+            uvWriter.addData2f(tex)
+            
+        triangles = GeomTriangles(Geom.UHStatic)
+        triangles.addVertices(0, 1, 2)
+        triangles.closePrimitive()
+        triangles.addVertices(2, 3, 0)
+        triangles.closePrimitive()
         
-        self.walkIval1 = self.panda.posInterval(2, Vec3(-8, -8, 0), startPos = Vec3(8, -8, 0))
-        self.walkIval2 = self.panda.posInterval(2, Vec3(-8, 8, 0), startPos = Vec3(-8, -8, 0))
-        self.walkIval3 = self.panda.posInterval(2, Vec3(8, 8, 0), startPos = Vec3(-8, 8, 0))
-        self.walkIval4 = self.panda.posInterval(2, Vec3(8, -8, 0), startPos = Vec3(8, 8, 0))
+        geom = Geom(geomData)
+        geom.addPrimitive(triangles)
+        node = GeomNode("box")
+        node.addGeom(geom)
+        box = render.attachNewNode(node)
+        texture = loader.loadTexture("../textures/crate.jpg")
+        box.setTexture(texture)
         
-        self.turnIval1 = self.panda.hprInterval(0.5, Vec3(180, 0, 0), startHpr = Vec3(270, 0, 0))
-        self.turnIval2 = self.panda.hprInterval(0.5, Vec3(90, 0, 0), startHpr = Vec3(180, 0, 0))
-        self.turnIval3 = self.panda.hprInterval(0.5, Vec3(0, 0, 0), startHpr = Vec3(90, 0, 0))
-        self.turnIval4 = self.panda.hprInterval(0.5, Vec3(-90, 0, 0), startHpr = Vec3(0, 0, 0))
-        
-        self.pandaWalk = Sequence(self.walkIval1, self.turnIval1, self.walkIval2, self.turnIval2, self.walkIval3, self.turnIval3, self.walkIval4, self.turnIval4)
-        
-        self.pandaWalk.loop()
-        
-        self.followCam = FollowCam(self.cam, self.panda)
-
+        self.cam.setPos(0, -5, 0)
