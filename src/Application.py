06c0238a936462394fa4957dfae86eee10f00b84
AJ Alves <zerokol@ymail.com>
2012-03-30 09:06:34 -0300

    loading models

--- src/Application.py
+++ src/Application.py
@@ -4,36 +4,17 @@ Created on Nov 17, 2011
 @author: zerokol
 '''
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import *
-
-vertices = [Vec3(1, 0, 1), Vec3(-1, 0, 1), Vec3(-1, 0, -1), Vec3(1, 0, -1)]
-texcoords = [Vec2(1, 1), Vec2(0, 1), Vec2(0, 0), Vec2(1, 0)]
+from direct.actor.Actor import Actor
+from panda3d.core import Vec3
 
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        
-        format = GeomVertexFormat.getV3t2()
-        geomData = GeomVertexData("box", format, Geom.UHStatic)
-        vertexWriter = GeomVertexWriter(geomData, "vertex")
-        uvWriter = GeomVertexWriter(geomData, "texcoord")
-        
-        for pos, tex in zip(vertices, texcoords):
-            vertexWriter.addData3f(pos)
-            uvWriter.addData2f(tex)
-            
-        triangles = GeomTriangles(Geom.UHStatic)
-        triangles.addVertices(0, 1, 2)
-        triangles.closePrimitive()
-        triangles.addVertices(2, 3, 0)
-        triangles.closePrimitive()
-        
-        geom = Geom(geomData)
-        geom.addPrimitive(triangles)
-        node = GeomNode("box")
-        node.addGeom(geom)
-        box = render.attachNewNode(node)
-        texture = loader.loadTexture("../textures/crate.jpg")
-        box.setTexture(texture)
-        
-        self.cam.setPos(0, -5, 0)
+        self.teapot = loader.loadModel("teapot")
+        self.teapot.reparentTo(render)
+        self.teapot.setPos(-5, 0, 0)
+        self.pandaActor = Actor("panda", {"walk": "panda-walk"})
+        self.pandaActor.reparentTo(render)
+        self.pandaActor.setPos(Vec3(5, 0, 0))
+        self.pandaActor.loop("walk")
+        self.cam.setPos(0, -30, 6)
