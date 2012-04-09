90fac154911462941398deaac4b0ad02f9faaae8
AJ Alves <zerokol@ymail.com>
2012-04-09 18:19:34 -0300

    pausa, audio

--- src/Application.py
+++ src/Application.py
@@ -3,33 +3,19 @@
     @author: zerokol
 '''
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import GeoMipTerrain
+from direct.showbase.Audio3DManager import Audio3DManager
 
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        # criando uma instância de um objeto GeoMipTerrain chamado "terrain"
-        self.terrain = GeoMipTerrain("terrain")
-        # configurando o objeto terrain com a imagem de campo de altura chamada height.png
-        self.terrain.setHeightfield("../textures/height.png")
-        # aplicando a textura do terreno
-        self.terrain.setColorMap("../textures/grass.png")
-        # Configurando a escala de variação do eixo Z na construção do terreno
-        self.terrain.getRoot().setSz(50)
-        # adicionando o objeto terrain a raiz da cena gráfica
-        self.terrain.getRoot().reparentTo(render)
-        # efetivamente gerando matemática e geometricamente o terreno
-        self.terrain.generate()
-        # capiturando a elevação na coordenada (256,256) do terreno
-        # e multiplicando por a elevação máxima do terreno somado com um valor de segurança para maior precisão
-        z = self.terrain.getElevation(256, 256) * (50 + 10)
-        # posicionando a câmera padrão no centro do terreno a uma altura maior que o ponto mais alto
-        self.cam.setPos(256, 256, z)
-        # informando que o ponto de foco será na região onde a câmera está
-        self.terrain.setFocalPoint(self.cam)
-        # Tarefa para atualizar sempre o ponto de foco do terreno, a medida que a ãmera se movimenta
-        self.taskMgr.add(self.updateTerrain, "update terrain")
-        
-    def updateTerrain(self, task):
-        self.terrain.update()
-        return task.cont
+        # carregando um Model simples utilizando o objeto loader
+        self.smiley = loader.loadModel("smiley")
+        # adicionando o objeto smaley a raiz da cena gráfica
+        self.smiley.reparentTo(render)
+        self.audio = Audio3DManager(self.sfxManagerList[0])
+        self.audio.attachListener(self.cam)
+        self.loop = self.audio.loadSfx("../sounds/loop.wav")
+        self.loop.setLoop(True)
+        self.audio.attachSoundToObject(self.loop, self.smiley)
+        self.loop.play()
+        self.cam.setPos(0, -40, 0)
