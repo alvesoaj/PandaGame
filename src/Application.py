044572f0bd81e3facb334be5f6f7be9d2aed1439
AJ Alves <zerokol@ymail.com>
2012-04-09 10:59:08 -0300

    rebuild

--- src/Application.py
+++ src/Application.py
@@ -1,20 +1,28 @@
+# -*- coding: UTF-8 -*-
 '''
-Created on Nov 17, 2011
-
-@author: zerokol
+    @author: zerokol
 '''
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
-from panda3d.core import Vec3
+# from panda3d.core import Vec3
 
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        # importando um objeto "Model", teapot, com auxílio do objeto "loader"
         self.teapot = loader.loadModel("teapot")
+        # adicionando o objeto teapot ao objeto raiz, "render"
         self.teapot.reparentTo(render)
+        # posicionando o objeto teapot cinco unidades espacialmente a esquerda
         self.teapot.setPos(-5, 0, 0)
+        # importando um objeto "Actor" panda, e seus movimentos
         self.pandaActor = Actor("panda", {"walk": "panda-walk"})
+        # adicionando o objeto pandaActor ao objeto raiz, "render"
         self.pandaActor.reparentTo(render)
-        self.pandaActor.setPos(Vec3(5, 0, 0))
+        # posicionando o objeto pandaActor cinco unidades espaciais a direita
+        # self.pandaActor.setPos(Vec3(5, 0, 0))
+        self.pandaActor.setPos(5, 0, 0)
+        # colocando o ator pandaActor em loop na animação "walk"
         self.pandaActor.loop("walk")
+        # configurando a camera padrão "cam" para a posição apropriada
         self.cam.setPos(0, -30, 6)
