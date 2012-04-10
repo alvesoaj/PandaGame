d3f0b4e60a258d991720a2ee6fd6e72e11781b61
AJ Alves <zerokol@ymail.com>
2012-04-10 14:58:50 -0300

    fim, som

--- src/Application.py
+++ src/Application.py
@@ -12,10 +12,17 @@ class Application(ShowBase):
         self.smiley = loader.loadModel("smiley")
         # adicionando o objeto smaley a raiz da cena gráfica
         self.smiley.reparentTo(render)
+        # insranciando um objeto Audio3DManager para criar efeitos
         self.audio = Audio3DManager(self.sfxManagerList[0])
+        # configurando para a câmera principal ser o objeto ao quel o som irá se relacionar
         self.audio.attachListener(self.cam)
+        # criando um objeto AudioSound a partir do objeto Audio3DManager 
         self.loop = self.audio.loadSfx("../sounds/loop.wav")
+        # configurando o som em loop infinito
         self.loop.setLoop(True)
+        # vinculando o objeto loop ao Model smiley, o som partirá de smiley
         self.audio.attachSoundToObject(self.loop, self.smiley)
+        # iniciando a reprodução do som
         self.loop.play()
+        # configurando a posição da câmera padrão no espaço
         self.cam.setPos(0, -40, 0)
