743363098cab46186d838ae0f39bc0e011e9fbfd
AJ Alves <zerokol@ymail.com>
2012-04-12 14:46:29 -0300

    making animations fit to instervals

--- src/Application.py
+++ src/Application.py
@@ -6,7 +6,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.showbase.RandomNumGen import RandomNumGen
 from direct.actor.Actor import Actor
 from panda3d.core import Vec3
-from direct.interval.IntervalGlobal import Func, Sequence
+from direct.interval.IntervalGlobal import Func, Sequence, ActorInterval, Parallel
 
 class Application(ShowBase):
     def __init__(self):
@@ -17,8 +17,6 @@ class Application(ShowBase):
         self.panda.reparentTo(render)
         # rotacionanado espacialmente o ator panda
         self.panda.setHpr(-90, 0, 0)
-        # iniciando a animação walk no ator panda
-        self.panda.loop("walk")
         
         # configurando o movimento de ir
         self.walkIval1 = self.panda.posInterval(2, Vec3(-8, 0, 0), startPos = Vec3(8, 0, 0))
@@ -30,8 +28,13 @@ class Application(ShowBase):
         self.turnIval2 = self.panda.hprInterval(0.5, Vec3(-90, 0, 0), startHpr = Vec3(90, 0, 0))
         # criando uma função de intervalo
         self.colorIval = Func(self.randomColor)
+        # instanciando um ActorInterval para gerendiar a animação do ator panda
+        self.pandaAnim = ActorInterval(self.panda, "walk", loop = 1, duration = 5)
+        # self.pandaAnim = ActorInterval(self.panda, "walk", startTime = 0, endTime = 1, playRate = 0.1, constrainedLoop = 1, duration = 5)
         # instanciando um objerto Sequence para coordenar e repetir os intervalos criados
-        self.pandaWalk = Sequence(self.walkIval1, self.turnIval1, self.colorIval, self.walkIval2, self.turnIval2, self.colorIval)
+        self.pandaMove = Sequence(self.walkIval1, self.turnIval1, self.colorIval, self.walkIval2, self.turnIval2, self.colorIval)
+        # instanciando um objeto Parallel para coordenar a animação e os movimentos do ator panda
+        self.pandaWalk = Parallel(self.pandaAnim, self.pandaMove)
         # colocando o objeto pandaWalk em loop infinito
         self.pandaWalk.loop()
         
