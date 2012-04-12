c53527b353af7b8cdeeab5d4817253880da846ed
AJ Alves <zerokol@ymail.com>
2012-04-12 12:11:03 -0300

    controlling actions using intervals

--- src/Application.py
+++ src/Application.py
@@ -3,76 +3,41 @@
     @author: zerokol
 '''
 from direct.showbase.ShowBase import ShowBase
+from direct.showbase.RandomNumGen import RandomNumGen
 from direct.actor.Actor import Actor
-#from panda3d.core import Vec3
+from panda3d.core import Vec3
+from direct.interval.IntervalGlobal import Func, Sequence
 
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        # carregando o modelo chamado sun
-        self.sun = loader.loadModel("smiley")
-        # relacionando o modelo sun a raiz da cena gráfica
-        self.sun.reparentTo(render)
-        # dimensionanco o modelo sun para uma escala de cinco vezes
-        self.sun.setScale(5)
-        
-        # carregando um modelo chamado phanton
-        self.phantom = loader.loadModel("teapot")
-        # ralacionando o modelo phanton ao modelo sun
-        self.phantom.reparentTo(self.sun)
-        # dimensionando a escala do modelo phantom para zero ponto um
-        self.phantom.setScale(0.1)
-        # posicionando espacialmente o modelo phantom
-        self.phantom.setPos(0, -5, 0)
-        # configurando o objeto phantom como invisivel
-        self.phantom.hide()
-        
-        # carregando um modelo chamdao earth
-        self.earth = loader.loadModel("frowney")
-        # criando uma nova cena gráfica chamada earthCenter
-        self.earthCenter = render.attachNewNode("earthCenter")
-        # relacionando o modelo earth a cena gráfica earthCenter
-        self.earth.reparentTo(self.earthCenter)
-        # posicionando espacialmente o modelo earth
-        self.earth.setPos(20, 0, 0)
-        
         # carregando um ator chamado panda
         self.panda = Actor("panda", {"walk": "panda-walk"})
-        # relacionando o ator panda ao modelo earth
-        self.panda.reparentTo(self.earth)
-        # dimensionando o ator panda para uma escala de zero ponto um
-        self.panda.setScale(0.1)
-        # posicionando espacialmente o ator panda
-        self.panda.setPos(0.7, 0, 0.7)
-        # posicionando direcionalmente o ator panda
-        self.panda.setHpr(0, 0, 40)
-        # inicinando a animação walk do ator panda
+        # relacionando o ator panda a cena gráfica raiz
+        self.panda.reparentTo(render)
+        # rotacionanado espacialmente o ator panda
+        self.panda.setHpr(-90, 0, 0)
+        # iniciando a animação walk no ator panda
         self.panda.loop("walk")
         
-        # carregando um modelo chamado moon
-        self.moon = loader.loadModel("box")
-        # criando uma nova cena gráfica chamada moonCenter
-        self.moonCenter = self.earthCenter.attachNewNode("moonCenter")
-        # relacionando o modelo moon a cena gráfica moonCenter
-        self.moon.reparentTo(self.moonCenter)
-        # posicionando espacialmente a cena gráfica moonCenter na mesma posição do modelo earth 
-        self.moonCenter.setPos(self.earth.getPos())
-        # posicionando espacialmente o modelo moon
-        self.moon.setPos(0, 0, 6)
+        # configurando o movimento de ir
+        self.walkIval1 = self.panda.posInterval(2, Vec3(-8, 0, 0), startPos = Vec3(8, 0, 0))
+        # configurando o movimento de voltar
+        self.walkIval2 = self.panda.posInterval(2, Vec3(8, 0, 0), startPos = Vec3(-8, 0, 0))
+        # configurando o movimento de virar a esquerda
+        self.turnIval1 = self.panda.hprInterval(0.5, Vec3(90, 0, 0), startHpr = Vec3(-90, 0, 0))
+        # configurando o movimento de virar a direita
+        self.turnIval2 = self.panda.hprInterval(0.5, Vec3(-90, 0, 0), startHpr = Vec3(90, 0, 0))
+        # criando uma função de intervalo
+        self.colorIval = Func(self.randomColor)
+        # instanciando um objerto Sequence para coordenar e repetir os intervalos criados
+        self.pandaWalk = Sequence(self.walkIval1, self.turnIval1, self.colorIval, self.walkIval2, self.turnIval2, self.colorIval)
+        # colocando o objeto pandaWalk em loop infinito
+        self.pandaWalk.loop()
         
-        # posicionando espacialmente a camera padrão
-        self.cam.setPos(0, -100, 0)
-        # adiciononando uma tarefa a aplicação
-        self.taskMgr.add(self.update, "update")
-        
-    def update(self, task):
-        # rotacionando o modelo sun no eixo x em dez unidades vezes o tempo total des de que a tarefa foi iniciada
-        self.sun.setP(task.time * 10)
-        # rotacionando o modelo earth no eixo z em menos cem unidades vezes o tempo total da tarefa
-        self.earth.setH(task.time * -100)
-        # rotacionado a cena gráfica earthCenter no eixo z em cinquenta unidades vezes o tempo total da tarefa
-        self.earthCenter.setH(task.time * 50)
-        # rotacionando a cena gráfica moonCenter no eixo y  em cento e cinquenta unidadess vezes o tempo toral da tarefa
-        self.moonCenter.setR(task.time * 150)
-        # retornando a tarefa para garantir continuidade
-        return task.cont
+        # posicionando espacialmente a camera principal
+        self.cam.setPos(0, -50, 6)
+    
+    def randomColor(self):
+        rand = RandomNumGen(globalClock.getFrameTime())
+        self.panda.setColorScale(rand.random(), rand.random(), rand.random(), 255)
