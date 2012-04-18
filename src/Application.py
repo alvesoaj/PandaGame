0da6dc4d15b3c57a35922d3ba6a63b740a28484f
AJ Alves <zerokol@ymail.com>
2012-04-18 10:26:23 -0300

    ok

--- src/Application.py
+++ src/Application.py
@@ -11,9 +11,15 @@ texcoords = [Vec2(1, 1), Vec2(0, 1), Vec2(0, 0), Vec2(1, 0)]
 class Application(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        # criando um objeto GeomVertexFormat e selecionando o formato posição em 3 pontos e textura em 2
         gvformat = GeomVertexFormat.getV3t2()
+        # criando um objeto GeomVertexData no formato gvformat, e sinalizando que ele não sofrerá transformações
         geomData = GeomVertexData("box", gvformat, Geom.UHStatic)
+        
+        # criando dois objetos GeomVertexWriter
+        # um para desenhar a malha
         vertexWriter = GeomVertexWriter(geomData, "vertex")
+        # o outro para desenhar a textura
         uvWriter = GeomVertexWriter(geomData, "texcoord")
         for pos, tex in zip(vertices, texcoords):
             vertexWriter.addData3f(pos)
